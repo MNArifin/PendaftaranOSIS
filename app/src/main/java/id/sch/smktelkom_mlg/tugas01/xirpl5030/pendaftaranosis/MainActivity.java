56296cd1afeffef2a1875711f3a9916885094dba
septy <septydarmantya@gmail.com>
2016-09-11 10:15:58 +0700

    menambahkan coding edit text

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5030/pendaftaranosis/MainActivity.java
@@ -0,0 +1,42 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl5030.pendaftaranosis;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    TextView tvHasil;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+
+        findViewById(R.id.buttonDaftar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
+        String nama = etNama.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+        } else if (nama.length() <= 3) {
+            etNama.setError("Nama minimal 3 karakter");
+        } else {
+            etNama.setError(null);
+        }
+        tvHasil.setText("Nama          : " + nama);
+    }
+}
